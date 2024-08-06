365b0bf930c8738eeb82bfac99e21ec23791598d
tengise174 <117653225+tengise174@users.noreply.github.com>
2024-08-06 04:36:38 -0500

    Remove Element

--- 21.MergeTwoSortedLists.js
+++ 21.MergeTwoSortedLists.js
@@ -1,3 +1,8 @@
+// ==== Completed ==== //
+
+// Youtubees husav
+
+
 /*
 You are given the heads of two sorted linked lists list1 and list2.
 
@@ -18,11 +23,28 @@ Return the head of the merged linked list.
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1) {
-    let list = new ListNode(0);
-    // list.next = list1;
-    console.log(typeof (list));
-    console.log(JSON.stringify(list1));
+var mergeTwoLists = function (list1, list2) {
+    let curr = new ListNode();
+    const dummy = curr;
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        }
+        else {
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next;
+    }
+    if (list1) {
+        curr.next = list1;
+    }
+    if (list2) {
+        curr.next = list2;
+    }
+
+    return dummy.next;
 };
 
 mergeTwoLists([1, 2, 4]);
